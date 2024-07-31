c14c10eaae5b56d9c2f5d2f5a47d26fec05ab399
Luiza <l.uhzinha@hotmail.com>
2024-07-31 14:25:47 -0300

    disable user_reload and add pid to debug

--- run.py
+++ run.py
@@ -2,5 +2,9 @@ from app import create_app
 
 app = create_app()
 
+import os
+
+print(f"Starting server with PID: {os.getpid()}")
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
