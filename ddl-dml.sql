7672f5a3e78bf758e4f53976d9b8a53b01157541
Luiza <l.uhzinha@hotmail.com>
2024-07-31 14:24:55 -0300

    sql database

--- ddl-dml.sql
+++ ddl-dml.sql
@@ -1,7 +1,7 @@
 START TRANSACTION;
 DROP TABLE IF EXISTS user;
 CREATE TABLE IF NOT EXISTS user (
-    id_user INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(100) NOT NULL,
     email VARCHAR(40) NOT NULL UNIQUE,
     password VARCHAR(120) NOT NULL,
@@ -11,39 +11,45 @@ CREATE TABLE IF NOT EXISTS user (
 INSERT INTO user VALUES (1,'','admin','admin','admin');
 
 
-CREATE TABLE Lockers (
+CREATE TABLE locker (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL
+    name VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE Compartments (
+CREATE TABLE compartment (
     id INT AUTO_INCREMENT PRIMARY KEY,
     locker_id INT NOT NULL,
-    numero INT NOT NULL,
-    FOREIGN KEY (locker_id) REFERENCES Lockers(id)
+    number INT NOT NULL,
+    FOREIGN KEY (locker_id) REFERENCES locker(id)
+    UNIQUE (locker_id, number)
 );
 
-CREATE TABLE CompartmentUsage (
+CREATE TABLE compartment_usage (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     compartment_id INT NOT NULL,
-    data_abertura DATETIME NOT NULL,
-    data_retirada DATETIME,
-    FOREIGN KEY (user_id) REFERENCES Users(id),
-    FOREIGN KEY (compartment_id) REFERENCES Compartments(id),
+    open_time DATETIME NOT NULL,
+    close_time DATETIME NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id_user),
+    FOREIGN KEY (compartment_id) REFERENCES compartment(id),
     UNIQUE (user_id)  -- Garante que um usuário só possa usar um compartimento por vez.
 );
 
-CREATE TABLE locker_schedules (
+CREATE TABLE locker_schedule (
     id INT AUTO_INCREMENT PRIMARY KEY,
     compartment_id INT NOT NULL,
-	user_id INT,
-    open_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-    close_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-    retrieve_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-    end_retrieve_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (compartment_id) REFERENCES lockers(locker_id),
-	FOREIGN KEY (user_id) REFERENCES Users(id),
+	user_id INT NOT NULL,
+    open_time DATETIME NOT NULL,
+    close_time DATETIME NOT NULL,
+    retrieve_time DATETIME NOT NULL,
+    end_retrieve_time DATETIME NOT NULL,
+    FOREIGN KEY (compartment_id) REFERENCES compartment(id),
+	FOREIGN KEY (user_id) REFERENCES user(id_user)
 );
 
 COMMIT;
+
+drop table compartment_usage;
+drop table locker_schedule;
+drop table compartment;
+drop table locker;
